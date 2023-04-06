25e5d9620366c8117afa5a786c55b48f669a388a
Elias <elias.ihma.gmail.com>
2023-04-07 01:44:02 +0200

    fix products and few things

--- src/data/index.ts
+++ src/data/index.ts
@@ -8,12 +8,26 @@ const accueil = data["accueil"];
 const products = data["products"];
 const categories = Object.keys(products);
 
+const productList = Object.entries(products)
+  .map(([category, productRanges]) => {
+    return Object.entries(productRanges).map(([range, products]) => {
+      return products.map((product) => {
+        return {
+          ...product,
+          category,
+          range,
+        };
+      });
+    });
+  })
+  .flat(3);
+
 type Accueil = typeof accueil;
 type Products = typeof products;
 type Categories = typeof categories;
 
 export type { Accueil, Products, Categories };
 
-export { accueil, products, categories };
+export { accueil, products, categories, productList };
 
 export default data;
